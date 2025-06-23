e4279ba9181165cd82243e694564da345ac28436
mesxdez <mesxdez@gmail.com>
2025-06-23 11:03:59 +0700

    update format date

--- src/app/(DashboardLayout)/components/dashboard/ContentList.tsx
+++ src/app/(DashboardLayout)/components/dashboard/ContentList.tsx
@@ -269,7 +269,11 @@ const ContentList = () => {
   };
 
   const formatDate = (dateString: string) => {
-    return new Date(dateString).toLocaleDateString();
+    const date = new Date(dateString);
+    const day = date.getDate().toString().padStart(2, "0");
+    const month = (date.getMonth() + 1).toString().padStart(2, "0");
+    const year = date.getFullYear();
+    return `${day}/${month}/${year}`;
   };
 
   const truncateHtml = (html: string, maxLength: number = 50) => {
